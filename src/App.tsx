4df06a54af2b8012e90fd34c4d38eff139be7d42
Ellis Pae <ellis.h.pae@gmail.com>
2024-12-31 18:18:12 -0500

    establish event handlers for To Do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useState, ChangeEvent, FormEvent } from "react";
+import "./App.css";
+import Todo from "./types/Todo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddTodo = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    if (!inputValue.trim()) return;
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      text: inputValue,
+      completed: false,
+    };
+
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+    setInputValue("");
+  };
+
+  const handleToggleComplete = (id: number): void => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const handleDeleteTodo = (id: number): void => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+};
 
 export default App;
